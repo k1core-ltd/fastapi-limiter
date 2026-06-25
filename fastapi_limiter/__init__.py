96c42d8afcfa439e4a8925c2fc271ee208825306
smiling-watermelon <146741211+smiling-watermelon@users.noreply.github.com>
2026-06-25 08:02:38 +0000

    fix: Support FastAPI of version 0.137+

--- fastapi_limiter/__init__.py
+++ fastapi_limiter/__init__.py
@@ -1,5 +1,6 @@
+from collections.abc import Callable
 from math import ceil
-from typing import Callable, Optional, Union
+from typing import TYPE_CHECKING
 
 from fastapi import HTTPException
 from starlette.requests import Request
@@ -7,54 +8,13 @@ from starlette.responses import Response
 from starlette.status import HTTP_429_TOO_MANY_REQUESTS
 from starlette.websockets import WebSocket
 
+if TYPE_CHECKING:
+    from redis import Redis
 
-async def default_identifier(request: Union[Request, WebSocket]):
-    forwarded = request.headers.get("X-Forwarded-For")
-    if forwarded:
-        ip = forwarded.split(",")[0]
-    else:
-        ip = request.client.host
-    return ip + ":" + request.scope["path"]
-
-
-async def http_default_callback(request: Request, response: Response, pexpire: int):
-    """
-    default callback when too many requests
-    :param request:
-    :param pexpire: The remaining milliseconds
-    :param response:
-    :return:
-    """
-    expire = ceil(pexpire / 1000)
-    raise HTTPException(
-        HTTP_429_TOO_MANY_REQUESTS, "Too Many Requests", headers={"Retry-After": str(expire)}
-    )
-
-
-async def ws_default_callback(ws: WebSocket, pexpire: int):
-    """
-    default callback when too many requests
-    :param ws:
-    :param pexpire: The remaining milliseconds
-    :return:
-    """
-    expire = ceil(pexpire / 1000)
-    raise HTTPException(
-        HTTP_429_TOO_MANY_REQUESTS, "Too Many Requests", headers={"Retry-After": str(expire)}
-    )
-
-
-class FastAPILimiter:
-    redis = None
-    prefix: Optional[str] = None
-    lua_sha: Optional[str] = None
-    identifier: Optional[Callable] = None
-    http_callback: Optional[Callable] = None
-    ws_callback: Optional[Callable] = None
-    lua_script = """local key = KEYS[1]
+DEFAULT_LUA_SCRIPT = """
+local key = KEYS[1]
 local limit = tonumber(ARGV[1])
 local expire_time = ARGV[2]
-
 local current = tonumber(redis.call('get', key) or "0")
 if current > 0 then
  if current + 1 > limit then
@@ -66,23 +26,82 @@ if current > 0 then
 else
     redis.call("SET", key, 1,"px",expire_time)
  return 0
-end"""
+end
+"""
+
+
+async def default_identifier(request: Request | WebSocket) -> str:
+    forwarded = request.headers.get("X-Forwarded-For")
+    ip = forwarded.split(",")[0] if forwarded else request.client.host
+    return ip + ":" + request.scope["path"]
+
+
+async def http_default_callback(
+    request: Request, response: Response, pexpire: int
+) -> None:  # noqa: ARG001
+    """
+    default callback when too many requests
+    :param request:
+    :param pexpire: The remaining milliseconds
+    :param response:
+    :return:
+    """
+    expire = ceil(pexpire / 1000)
+    raise HTTPException(
+        HTTP_429_TOO_MANY_REQUESTS,
+        "Too Many Requests",
+        headers={"Retry-After": str(expire)},
+    )
+
+
+async def ws_default_callback(ws: WebSocket, pexpire: int) -> None:  # noqa: ARG001
+    """
+    default callback when too many requests
+    :param ws:
+    :param pexpire: The remaining milliseconds
+    :return:
+    """
+    expire = ceil(pexpire / 1000)
+    raise HTTPException(
+        HTTP_429_TOO_MANY_REQUESTS,
+        "Too Many Requests",
+        headers={"Retry-After": str(expire)},
+    )
+
+
+class FastAPILimiter:
+    redis: "Redis" = None
+    prefix: str | None = None
+    lua_sha: str | None = None
+    identifier: Callable | None = None
+    http_callback: Callable | None = None
+    ws_callback: Callable | None = None
+    lua_script: str | None = None
 
     @classmethod
-    async def init(
+    async def init(  # noqa: PLR0913
         cls,
-        redis,
+        redis,  # noqa: ANN001
         prefix: str = "fastapi-limiter",
         identifier: Callable = default_identifier,
         http_callback: Callable = http_default_callback,
         ws_callback: Callable = ws_default_callback,
+        lua_script: str | None = None,
     ) -> None:
         cls.redis = redis
+
+        assert hasattr(cls.redis, "evalsha"), (
+            "Redis client must support evalsha command"
+        )
+        assert hasattr(cls.redis, "script_load"), (
+            "Redis client must support script_load command"
+        )
+
         cls.prefix = prefix
         cls.identifier = identifier
         cls.http_callback = http_callback
         cls.ws_callback = ws_callback
-        cls.lua_sha = await redis.script_load(cls.lua_script)
+        cls.lua_sha = await redis.script_load(lua_script or DEFAULT_LUA_SCRIPT)
 
     @classmethod
     async def close(cls) -> None:
--- fastapi_limiter/depends.py
+++ fastapi_limiter/depends.py
@@ -1,45 +1,84 @@
-from typing import Annotated, Callable, Optional
+from collections.abc import Callable
+from typing import Annotated
 
 import redis as pyredis
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import Response
+from fastapi.routing import APIRoute, _IncludedRouter
 from pydantic import Field
-from starlette.requests import Request
-from starlette.responses import Response
+from starlette.routing import Match, Route
 from starlette.websockets import WebSocket
 
 from fastapi_limiter import FastAPILimiter
 
 
+def _flatten_routes(app: "FastAPI") -> list[APIRoute | Route]:
+    routes: list[APIRoute | Route] = []
+    for route in app.routes:
+        if isinstance(route, (APIRoute, Route)):
+            routes.append(route)
+        elif hasattr(route, "original_router"):
+            route: _IncludedRouter
+            routes.extend(_flatten_routes(route.original_router))
+        else:
+            raise Exception(f"Unknown route type: {type(route)}")
+    return routes
+
+
 class RateLimiter:
-    def __init__(
+    def __init__(  # noqa: PLR0913
         self,
         times: Annotated[int, Field(ge=0)] = 1,
         milliseconds: Annotated[int, Field(ge=-1)] = 0,
         seconds: Annotated[int, Field(ge=-1)] = 0,
         minutes: Annotated[int, Field(ge=-1)] = 0,
         hours: Annotated[int, Field(ge=-1)] = 0,
-        identifier: Optional[Callable] = None,
-        callback: Optional[Callable] = None,
-    ):
+        identifier: Callable | None = None,
+        callback: Callable | None = None,
+    ) -> None:
         self.times = times
-        self.milliseconds = milliseconds + 1000 * seconds + 60000 * minutes + 3600000 * hours
+        self.milliseconds = (
+            milliseconds + 1000 * seconds + 60000 * minutes + 3600000 * hours
+        )
         self.identifier = identifier
         self.callback = callback
 
-    async def _check(self, key):
+    async def _check(self, key: bytes | str | memoryview | float) -> str:
         redis = FastAPILimiter.redis
-        pexpire = await redis.evalsha(
-            FastAPILimiter.lua_sha, 1, key, str(self.times), str(self.milliseconds)
+        return await redis.evalsha(
+            FastAPILimiter.lua_sha,
+            1,
+            key,
+            str(self.times),
+            str(self.milliseconds),
         )
-        return pexpire
 
-    async def __call__(self, request: Request, response: Response):
+    async def __call__(
+        self,
+        request: Request,
+        response: Response,
+    ) -> None:
         if not FastAPILimiter.redis:
-            raise Exception("You must call FastAPILimiter.init in startup event of fastapi!")
+            raise Exception(
+                "You must call FastAPILimiter.init in startup event of fastapi!"
+            )
+
         route_index = 0
         dep_index = 0
-        for i, route in enumerate(request.app.routes):
-            if route.path == request.scope["path"] and request.method in route.methods:
+
+        assert isinstance(request.app, FastAPI)
+
+        for i, route in enumerate(_flatten_routes(request.app)):
+            match, _ = route.matches(scope=request.scope)
+            if match == Match.FULL:
                 route_index = i
+
+                if not hasattr(route, "dependencies"):
+                    continue
+
+                assert isinstance(route, APIRoute)
+
                 for j, dependency in enumerate(route.dependencies):
                     if self is dependency.dependency:
                         dep_index = j
@@ -57,18 +96,31 @@ class RateLimiter:
                 FastAPILimiter.lua_script
             )
             pexpire = await self._check(key)
+
         if pexpire != 0:
             return await callback(request, response, pexpire)
 
+        return None
+
 
 class WebSocketRateLimiter(RateLimiter):
-    async def __call__(self, ws: WebSocket, context_key=""):
+    async def __call__(
+        self,
+        ws: WebSocket,
+        context_key: str = "",
+    ) -> None:
         if not FastAPILimiter.redis:
-            raise Exception("You must call FastAPILimiter.init in startup event of fastapi!")
+            raise Exception(
+                "You must call FastAPILimiter.init in startup event of fastapi!"
+            )
+
         identifier = self.identifier or FastAPILimiter.identifier
         rate_key = await identifier(ws)
         key = f"{FastAPILimiter.prefix}:ws:{rate_key}:{context_key}"
         pexpire = await self._check(key)
         callback = self.callback or FastAPILimiter.ws_callback
+
         if pexpire != 0:
             return await callback(ws, pexpire)
+
+        return None
